309b9c2f882149a334cf47a8ddf5f2272a880580
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-11 13:12:40 -0700

    WIP: Start Markov Dictionary Defining

---
+++ markov.py
@@ -0,0 +1,84 @@
+"""Generate Markov text from text files."""
+
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Take file path as string; return text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    # your code goes here
+    contents = open(file_path).read()
+    text = contents.split()
+    return text
+
+
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains("hi there mary hi there juanita")
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+
+        >>> chains[('there','juanita')]
+        [None]
+    """
+
+    chains = {}
+
+    # your code goes here
+    #value_list = []
+    for i in range(len(text_string) - 2):
+        key1 = (text_string[i], text_string[i + 1])
+        value1 = i + 2
+        #chains[key1] = text_string[value1]
+        # if the tuple key is not in dictionary, add key, value
+        if key1 not in chains:
+            chains[key1] = [text_string[value1]]
+        # if not, append to value list
+        else:
+            chains[key1].append(value1)
+            chains[key1] = text_string[value1]
+    return chains
+
+# print make_chains("green-eggs.eend.txt")
+
+def make_text(chains):
+    """Return text from chains."""
+
+    words = []
+
+    # your code goes here
+
+    return " ".join(words)
+
+
+input_path = "green-eggs.txt"
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print random_text
